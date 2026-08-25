faf791588e9cdcfcf4f682c06db1eb04978e1176
Chidush <tobiadejumo10@gmail.com>
2026-08-25 14:24:58 -0500

    chore: add exports to index file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { createFlashcards } from './flashcard/create-flashcards.js';
+export type { Flashcard, CreateFlashcardsOptions } from './flashcard/create-flashcards.js';
+
+export { createNote } from './notes/create-note.js';
+export type { Note, CreateNoteOptions } from './notes/create-note.js';
+
+export { createQuiz } from './quizzes/create-quiz.js';
+export type { Quiz, QuizQuestion, CreateQuizOptions } from './quizzes/create-quiz.js';
+
+export type { Difficulty, GenerationOptions } from './shared/types.js';
