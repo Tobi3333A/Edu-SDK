216fd5b96e937399d267c2102a3e9a7de3880f7a
Chidush <tobiadejumo10@gmail.com>
2026-08-25 09:04:10 -0500

    feat: Added flashcard function

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
+import { generateText, Output } from 'ai';
+import { z } from 'zod';
+
+
 export type Flashcard = {
     front: string;
     back: string;
 };
 
-export function createFlashcard(
-    front: string,
-    back: string
-): Flashcard {
-    return {
-        front,
-        back
-    };
+export type Flashcards = Flashcard[]
+
+export type CreateFlashcardsOptions = {
+    content: string;
+    model: string;
+    difficulty?: 'easy' | 'medium' | 'hard',
+    count: number
+}
+
+export async function createFlashcards({ content, model, difficulty = 'medium', count }: CreateFlashcardsOptions): Promise<Flashcards> {
+   const { output }  = await generateText({
+        model,
+        prompt: `Create ${count} flashcards from this content: ${content} and with this difficulty level: ${difficulty}`,
+        output: Output.array({
+            element: z.object({
+                front: z.string().describe('The front part of a flash card'),
+                back: z.string().describe('The back part of a flash card')
+            })
+        })
+   });
+
+   return output;
 }
